3301d063d561145d9496844f1da4ad17b0493643
Amadeusz Blanik <amadeusz@blanik.me>
2020-12-11 00:06:59 +0100

    Enabled all cors for production environment

--- src/app.ts
+++ src/app.ts
@@ -38,7 +38,7 @@ class App {
       this.app.use(hpp());
       this.app.use(helmet());
       this.app.use(logger('combined'));
-      this.app.use(cors({ origin: 'your.domain.com', credentials: true }));
+      this.app.use(cors({ origin: true, credentials: true }));
     } else {
       this.app.use(logger('dev'));
       this.app.use(cors({ origin: true, credentials: true }));
